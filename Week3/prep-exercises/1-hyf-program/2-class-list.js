880752f07c67c28cdb98d461ce77c12b85684edf
Erhan Koral <erhan.16k@gmail.com>
2023-11-16 22:23:54 +0100

    class-list exercise completed

--- Week3/prep-exercises/1-hyf-program/2-class-list.js
+++ Week3/prep-exercises/1-hyf-program/2-class-list.js
@@ -12,10 +12,33 @@ import { modules, students, mentors, classes } from "./hyf.js";
  *  [{ name: 'John', role: 'student' }, { name: 'Mary', role: 'mentor' }]
  */
 const getPeopleOfClass = (className) => {
-  // TODO complete this function
-};
+  const studentsOfClass = students
+    .filter(student => student.class === className)
+    .map(student => {
+      return {
+        name: student.name,
+        role: 'student'
+      }
+    })
+
+    let currentModule; 
+    classes.map(classItem => {
+        if(classItem.name === className && classItem.active === true) {
+           currentModule = classItem.currentModule
+        }
+      })
+    const mentorsOfClass = mentors
+      .filter(mentor => mentor.nowTeaching && mentor.nowTeaching === currentModule)
+      .map(mentor => {
+        return {
+          name: mentor.name,
+          role: 'mentor'
+        }
+      })
+      return currentModule ? [...studentsOfClass, ...mentorsOfClass]: [...studentsOfClass]
+  };
 // You can uncomment out this line to try your function
-// console.log(getPeopleOfClass('class34'));
+//console.log(getPeopleOfClass('class34'));
 
 /**
  * We would like to have a complete overview of the current active classes.
@@ -30,7 +53,13 @@ const getPeopleOfClass = (className) => {
  *  }
  */
 const getActiveClasses = () => {
-  // TODO complete this function
+  const activeClasses = {}
+  classes
+    .filter(classItem => classItem.active === true)
+    .map(classItem => {
+      activeClasses[classItem.name] = getPeopleOfClass(classItem.name)
+    })
+    return activeClasses;
 };
 // You can uncomment out this line to try your function
-// console.log(getActiveClasses());
+console.log(getActiveClasses());
